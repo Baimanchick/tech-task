04f9e19fd19d162553a02e9e47d98fb36f37fca9
Baimanchick <baikaimanchick@gmail.com>
2026-01-06 16:56:27 +0600

    [FEAT] Created app and basic architecture of project. Installed all assets and linked font

---
+++ src/shared/assets/images/home/index.ts
@@ -0,0 +1,17 @@
+import Autumn from './autumn.png'
+import Background from './background.png'
+import Earth from './earth.png'
+import Spring from './spring.png'
+import Summer from './summer.png'
+import Sun from './sun.png'
+import Winter from './winter.png'
+
+export {
+  Autumn,
+  Background,
+  Earth,
+  Spring,
+  Summer,
+  Sun,
+  Winter,
+}
